2db121a80fbde60dbf7afdaedfb54c6407af0bf5
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-09-26 21:31:17 -0300

    agrego rutas a app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DetalleProducto from "./components/views/producto/DetalleProducto";
+import Home from "./components/views/Home";
+import AcercaDe from "./components/views/AcercaDe";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <body>
+      <BrowserRouter>
+        <header></header>
+        <Routes>
+          <Route exact path="/" element={<Home></Home>}></Route>
+          <Route
+            exact
+            path="/detalleProducto/:id"
+            element={<DetalleProducto></DetalleProducto>}
+          ></Route>
+          <Route exact path="/acercaDe" element={<AcercaDe></AcercaDe>}></Route>
+          {/* <Route exact path="*" element={<Error404></Error404>}></Route> */}
+        </Routes>
+        <footer></footer>
+      </BrowserRouter>
+    </body>
   );
 };
 
