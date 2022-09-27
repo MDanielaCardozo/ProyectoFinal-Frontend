5a4d364dc7912d1258d519f92b86f64414fb8b90
carlosmsx <carlosmsx@gmail.com>
2022-09-27 00:30:02 -0300

    Agregada la ruta a Login y el componente Login vacio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" element={<Login/>}></Route>
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 };
