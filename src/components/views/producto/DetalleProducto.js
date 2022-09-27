2a06372b3a83e660946d7fd9f7fa4138a818b63b
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-09-26 21:33:24 -0300

    modifico nombre de detalle producto

--- src/components/views/producto/DetalleProducto.js
+++ src/components/views/producto/DetalleProducto.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const DetalleProd = () => {
+const DetalleProducto = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const DetalleProd = () => {
     );
 };
 
-export default DetalleProd;
+export default DetalleProducto;
