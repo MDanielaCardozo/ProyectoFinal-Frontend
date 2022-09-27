eeee91610eff074cabd9e4ec87a1d7b44e3e3e5c
Juan Menichetti <jpmenichetti13@gmail.com>
2022-09-27 17:44:07 -0300

    se agrego el navbar y sus efectos hover sobre los link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Menu from './components/common/Menu';
+import Footer from './components/common/Footer';
+import Burger1 from './img/Burger1.jpg'
 
 const App = () => {
   return (
     <div>
-      
+      <Menu></Menu>
+      <img
+              alt=""
+              src={Burger1}
+              width="100%"
+              height="auto"
+              className=""
+            />{' '}
+      <Footer></Footer>
     </div>
   );
 };
