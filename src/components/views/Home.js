32d4b52323c75f3ad1214f53ce2b208fa05d2429
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-09-28 13:21:23 -0300

    agrego imagenes y maquetado inicial de portada

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import pared from "../../img/purplewall.jpg";
+import imgPared1 from "../../img/manohambur.jpg";
+import imgPared2 from "../../img/platohambur.jpg";
+import imgPared3 from "../../img/meatgood.jpg";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <main>
+      <section className="bannerPromo">
+        <img src={pared} alt="" className="fondoBrick" />
+        <Row className="rowBrick">
+          <Col md={3} className="colPromo">
+            <img src={imgPared1} alt="" className="imgPromo" />
+          </Col>
+          <Col md={3} className="colPromo">
+            <img src={imgPared3} alt="" className="imgPromo" />
+          </Col>
+          <Col md={3} className="colPromo">
+            <img src={imgPared2} alt="" className="imgPromo" />
+          </Col>
+        </Row>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
