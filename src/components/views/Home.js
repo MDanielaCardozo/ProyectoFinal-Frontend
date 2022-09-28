f07dabafce7a6b58b288ee5e362f52d5e37c07dc
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-09-28 18:09:30 -0300

    agrego imagenes y nueva section con texto e imagen

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import pared from "../../img/purplewall.jpg";
-import imgPared1 from "../../img/manohambur.jpg";
-import imgPared2 from "../../img/platohambur.jpg";
-import imgPared3 from "../../img/meatgood.jpg";
+import promo1 from "../../img/promo1.jpg";
+import promo2 from "../../img/promo2.jpg";
+import promo3 from "../../img/promo3.jpg";
+import meatgood from "../../img/meatgood.jpg"
 import "./Home.css";
 
 const Home = () => {
@@ -11,15 +12,29 @@ const Home = () => {
     <main>
       <section className="bannerPromo">
         <img src={pared} alt="" className="fondoBrick" />
-        <Row className="rowBrick">
+        <Row className="rowBrick m-0">
           <Col md={3} className="colPromo">
-            <img src={imgPared1} alt="" className="imgPromo" />
+            <img src={promo1} alt="" className="imgPromo" />
           </Col>
           <Col md={3} className="colPromo">
-            <img src={imgPared3} alt="" className="imgPromo" />
+            <img src={promo2} alt="" className="imgPromo" />
           </Col>
           <Col md={3} className="colPromo">
-            <img src={imgPared2} alt="" className="imgPromo" />
+            <img src={promo3} alt="" className="imgPromo" />
+          </Col>
+        </Row>
+      </section>
+      <section className="bannerConocenos">
+        <Row className="rowConocenos w-100 m-0">
+          <Col md={4} className="bannerTexto">
+            <div className="ms-5 text-light">
+              <h1>Somos expertos</h1>
+              <h2>Veni a conocernos</h2>
+              <Button variant="outline-info mt-2">Más sobre nosotros</Button>
+            </div>
+          </Col>
+          <Col md={8} className="h-100 m-0 p-0">
+            <img src={meatgood} alt="" className="imgConocenos" />
           </Col>
         </Row>
       </section>
