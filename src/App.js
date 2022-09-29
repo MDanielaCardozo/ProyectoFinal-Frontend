aa090ed48cea67cefb086d903ba1065c8d3767e5
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-28 21:36:00 -0300

    agregamos maquetado de la pagina de registro

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/views/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Registro from './components/views/Registro';
+
 const App = () => {
   return (
       <div>
           <BrowserRouter>
               <Routes>
                   <Route exact path="/login" element={<Login />}></Route>
-                  <Route exact path="/registro" element={<Registro />}></Route>
+                  <Route exact path="/registro" element={<Registro/>}></Route>
               </Routes>
           </BrowserRouter>
       </div>
--- src/components/views/Registro.jsx
+++ src/components/views/Registro.jsx
@@ -1,9 +1,83 @@
 import React from 'react';
-
+import { Form,Card,Button } from 'react-bootstrap';
+import './registro.css'
 const Registro = () => {
     return (
-        <div>
-            <h1>Pagina de registro</h1>
+        <div className="imagen py-5 position-fixed px-20">
+            <Card className="container my-5 bg-light  rounded bg-form  px-0">
+                <div className="bg-dark rounded py-2">
+                    <h1 className="fs-4 fw-light text-center text-light">
+                        Complete el formulario para Registrarse
+                    </h1>
+                </div>
+                <Form className="mb-3 container ">
+                    <div className="row py-4">
+                        <div className="col-12 col-md-6">
+                            <Form.Group
+                                className="mb-3 text-light"
+                                controlId="formAdmin"
+                            >
+                                <Form.Label>Nombre*</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ej: Admin"
+                                />
+                            </Form.Group>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <Form.Group
+                                className="mb-3 text-light"
+                                controlId="formEmail"
+                            >
+                                <Form.Label>email*</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ej: juanperez@gmail.com"
+                                />
+                            </Form.Group>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <Form.Group
+                                className="mb-3 text-light"
+                                controlId="formClave"
+                            >
+                                <Form.Label>Contraseña*</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ej: 1234admin"
+                                />
+                            </Form.Group>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <Form.Group
+                                className="mb-3 text-light"
+                                controlId="formEstado"
+                            >
+                                <Form.Label>Estado</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ej: estado"
+                                />
+                            </Form.Group>
+                        </div>
+                        <div className="col-12">
+                            <Form.Group
+                                className="mb-3 text-light"
+                                controlId="formPerfil"
+                            >
+                                <Form.Label>Perfil</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Ej: perfil"
+                                />
+                            </Form.Group>
+                        </div>
+                    </div>
+                    <Button variant="outline-light" type="submit">
+                        Ingresar
+                    </Button>
+                </Form>
+            </Card>
         </div>
     );
 };
