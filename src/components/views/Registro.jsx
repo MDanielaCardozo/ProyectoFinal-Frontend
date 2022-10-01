7e219641fcaf9bc9cb33e672fc0b345422ca77e2
nicolas andrade <nicolasandrade328@gmail.com>
2022-10-01 14:57:12 -0300

    creamos los hook para almacenar datos de entrada en el formulario

--- src/components/views/Registro.jsx
+++ src/components/views/Registro.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form,Card,Button } from 'react-bootstrap';
 import './registro.css'
 const Registro = () => {
+
+    const [nombre,setnombre]=useState('');
+    const [email,setemail]= useState('');
+    const [clave,setclave]= useState('');
+
     return (
         <main className="imagen justify-content-center  px-20  ">
             <Card className="container  rounded bg-form px-0">
@@ -10,7 +15,7 @@ const Registro = () => {
                         Complete el formulario para Registrarse
                     </h1>
                 </div>
-                <div className="container">
+                <Form className="container" >
                     <div className="row py-4">
                         <div className="col-12 ">
                             <Form.Group
@@ -21,6 +26,7 @@ const Registro = () => {
                                 <Form.Control
                                     type="text"
                                     placeholder="Ej: Admin"
+                                    onChange={(e) => setnombre(e.target.value)}
                                 />
                             </Form.Group>
                         </div>
@@ -33,6 +39,7 @@ const Registro = () => {
                                 <Form.Control
                                     type="text"
                                     placeholder="Ej: juanperez@gmail.com"
+                                    onChange={(e)=> setemail(e.target.value)}
                                 />
                             </Form.Group>
                         </div>
@@ -45,6 +52,7 @@ const Registro = () => {
                                 <Form.Control
                                     type="text"
                                     placeholder="Ej: 1234admin"
+                                    onChange={(e)=>setclave(e.target.value)}
                                 />
                             </Form.Group>
                         </div>
@@ -56,7 +64,7 @@ const Registro = () => {
                     >
                         Ingresar
                     </Button>
-                </div>
+                </Form>
             </Card>
         </main>
     );
