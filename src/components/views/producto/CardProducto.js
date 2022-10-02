3421e2089ef122f23a332cd072e9f6b0de7d4c57
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-10-02 15:25:17 -0300

    cambio de estilos y colores para la card producto

--- src/components/views/producto/CardProducto.js
+++ src/components/views/producto/CardProducto.js
@@ -7,20 +7,22 @@ import "./CardProducto.css";
 const CardProducto = () => {
   return (
     <Col md={3}>
-      <Card className="mb-5">
+      <Card className="mb-5 cardProd">
         <Card.Img variant="top" src={hambur} className="m-0 p-0 cardImg" />
         <Card.Body>
           <Card.Title>
             <h2>Pizza napolitana</h2>
           </Card.Title>
-          <Card.Text className="cardDescrip">Pizza napolitana con masa madre</Card.Text>
+          <Card.Text className="cardDescrip">
+            Pizza napolitana con masa madre
+          </Card.Text>
         </Card.Body>
         <Card.Footer>
           <Row className="text-center align-items-center justify-content-around d-flex flex-nowrap">
-            <Link to={`*`} className="btn btn-danger w-50">
+            <Link to={`*`} className="btn w-50 btnCard">
               Agregar al carrito
             </Link>
-            <Link to={`/detalleProducto`} className="btn btn-danger w-25">
+            <Link to={`/detalleProducto`} className="btn w-25 btnCard">
               Ver más
             </Link>
           </Row>
