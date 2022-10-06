5ede31a7380d240d1e5059bcd6a7e66f47a717e7
nicolas andrade <nicolasandrade328@gmail.com>
2022-10-05 23:07:49 -0300

    corregimos la repeticion de la carpeta common

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -14,54 +14,150 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <MDBFooter bgColor="dark" className="text-center text-lg-start footerLetra">
-      <section className="pt-1">
-        <MDBContainer className="text-center text-md-start mt-5">
-          <MDBRow className="mt-3">
-            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-              <img
-                alt="Logo del restaurante"
-                src={Logo}
-                width="250"
-                height="auto"
-                className="my-0 py-0 ps-auto"
-              />{" "}
-            </MDBCol>
+      <MDBFooter
+          bgColor="dark"
+          className="text-center text-lg-start footerLetra"
+      >
+          <section className="pt-1">
+              <MDBContainer className="text-center text-md-start mt-5">
+                  <MDBRow className="mt-3">
+                      <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+                          <img
+                              alt="Logo del restaurante"
+                              src={Logo}
+                              width="250"
+                              height="auto"
+                              className="my-0 py-0 ps-auto"
+                          />{' '}
+                      </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className=" fw-bold mb-4 fs-5">Conócenos</h6>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Contactanos</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Promociones</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Informacion legal</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Terminos y condiciones de uso</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Politica de privacidad</Link></p>
+                      <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+                          <h6 className=" fw-bold mb-4 fs-5">Conócenos</h6>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Contactanos
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Promociones
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Informacion legal
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Terminos y condiciones de uso
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Politica de privacidad
+                              </Link>
+                          </p>
+                      </MDBCol>
 
-            </MDBCol>
+                      <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+                          <h6 className="fw-bold mb-4 fs-5">Mi cuenta</h6>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Mis pedidos
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Burger points
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  Mi cuenta
+                              </Link>
+                          </p>
+                      </MDBCol>
 
-            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="fw-bold mb-4 fs-5">Mi cuenta</h6>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Mis pedidos</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Burger points</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado">Mi cuenta</Link></p>
+                      <MDBCol
+                          md="4"
+                          lg="3"
+                          xl="3"
+                          className="mx-auto mb-md-0 mb-4"
+                      >
+                          <h6 className="fw-bold mb-4 fs-5">Redes sociales</h6>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  <FontAwesomeIcon icon={faFacebook} /> Facebook
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  <FontAwesomeIcon icon={faInstagram} />{' '}
+                                  Instagram
+                              </Link>
+                          </p>
+                          <p>
+                              <Link
+                                  end
+                                  to="/"
+                                  className="fs-6 text-reset subrayado"
+                              >
+                                  <FontAwesomeIcon icon={faTwitter} /> Twitter
+                              </Link>
+                          </p>
+                      </MDBCol>
+                  </MDBRow>
+              </MDBContainer>
+          </section>
 
-            </MDBCol>
-
-            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
-              <h6 className="fw-bold mb-4 fs-5">Redes sociales</h6>
-              <p><Link end to='/' className="fs-6 text-reset subrayado"><FontAwesomeIcon icon={faFacebook}/> Facebook</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado"><FontAwesomeIcon icon={faInstagram}/> Instagram</Link></p>
-              <p><Link end to='/' className="fs-6 text-reset subrayado"><FontAwesomeIcon icon={faTwitter}/> Twitter</Link></p>
-
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
-
-      <div
-        className="text-center p-4"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-      >© Todos los derechos reservados.</div>
-    </MDBFooter>
+          <div
+              className="text-center p-4 "
+              style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+          >
+              © Todos los derechos reservados.
+          </div>
+      </MDBFooter>
   );
 };
 
