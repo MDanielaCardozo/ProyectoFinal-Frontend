25f76c64edd1ecae04d7dd53c8456916afbf682c
franAndrad <franciscoezequielandrade@gmail.com>
2022-10-06 00:32:38 -0300

    se incremento el tamaño de texto para el 404

--- src/components/views/Error.js
+++ src/components/views/Error.js
@@ -5,7 +5,7 @@ import imagen from "./error404.png"
 const Error = () => {
     return (
         <Container className='my-5'>
-            <h1 className='fs-4 font'> La pagina que estas buscando no pudo ser encontrada</h1>
+            {/* <h1 className='fs-4 font'> La pagina que estas buscando no pudo ser encontrada</h1> */}
             <section class="error-container">
                 <span><span>4</span></span>
                 <span><img src={imagen} alt="hamburguesa mordida" className='imagen'/></span>
