47340b515df1d1518c702fe58f5e748b43fb9132
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-10-11 12:36:46 -0300

    modifico cards height y estilos de textos en ellas

--- src/components/views/producto/CardProducto.js
+++ src/components/views/producto/CardProducto.js
@@ -5,17 +5,23 @@ import "./CardProducto.css";
 
 const CardProducto = (props) => {
   return (
-    <Col md={4} xl={3}>
-      <Card className="mb-5 cardProd">
+    <Col md={4} xl={3} className="mb-5">
+      <Card className="h-100 cardProd">
         <Card.Img
           variant="top"
           src={props.producto.imagen}
           className="m-0 p-0 cardImg"
         />
-        <Card.Body>
-          <Card.Title>
-            <h2>{props.producto.nombre}</h2>
+        <Card.Body className="cardProdBody">
+          <Card.Title className="m-0">
+            <h2 className="m-0">{props.producto.nombre}</h2>
           </Card.Title>
+          <div className="w-100">
+            <hr className="mt-2"></hr>
+            <Card.Subtitle>
+              <h2 className="mb-0 cardPrecio">${props.producto.precio}</h2>
+            </Card.Subtitle>
+          </div>
         </Card.Body>
         <Card.Footer>
           <Row className="text-center align-items-center justify-content-around d-flex flex-wrap">
