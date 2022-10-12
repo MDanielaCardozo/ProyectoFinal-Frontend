6bec1fa472e38b1c08ea98b6d7bf37a085ba92b3
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-10-12 01:51:19 -0300

    Agregué el btn para el modal en la Card Producto

--- src/components/views/producto/CardProducto.js
+++ src/components/views/producto/CardProducto.js
@@ -1,10 +1,18 @@
-import React from "react";
-import { Card, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./CardProducto.css";
+import DetalleProducto from "./DetalleProducto";
 
 const CardProducto = (props) => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
   return (
+    <>
     <Col md={4} xl={3} className="mb-5">
       <Card className="h-100 cardProd">
         <Card.Img
@@ -28,16 +36,19 @@ const CardProducto = (props) => {
             <Link to={`*`} className="btn btnCard btnCarrito">
               Agregar al carrito
             </Link>
-            <Link
-              to={`/detalleProducto/${props.producto._id}`}
+            <Button
               className="btn btnCard mt-1 mt-xl-0 btnVerMas"
+              type="submit"
+              onClick={handleShow}
             >
               Ver más
-            </Link>
+            </Button>
           </Row>
         </Card.Footer>
       </Card>
     </Col>
+    <DetalleProducto show={show} handleClose={handleClose}/>
+    </>
   );
 };
 
