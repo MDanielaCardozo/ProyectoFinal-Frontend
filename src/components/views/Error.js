2cfd4aa0edb225b70967fd0e8419c0c9792e5fa5
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-10-13 05:58:41 -0300

    modifico scrolls verticales y letras de pedidos

--- src/components/views/Error.js
+++ src/components/views/Error.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import './error.css'
-import imagen from "../../img/purplewall.jpg"
+import React, { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import "./error.css";
+import imagen from "../../img/purplewall.jpg";
 const Error = () => {
-    return (
-        <section className='fondo'>
-            <div className="logo"><b><span>4</span>0<span>4</span></b></div>
-            <h3 className='notfound'> Esta pagina no funciona</h3>
-        </section>
-
-    );
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <section className="fondo">
+      <div className="logo">
+        <b>
+          <span>4</span>0<span>4</span>
+        </b>
+      </div>
+      <h3 className="notfound"> Esta pagina no funciona</h3>
+    </section>
+  );
 };
 
 export default Error;
--- src/components/views/ItemPedidos.js
+++ src/components/views/ItemPedidos.js
@@ -6,10 +6,10 @@ import Button from 'react-bootstrap/Button';
 const ItemPedidos = ({producto, quitarProducto}) => {
     return (
          <tr>
-              <td>{producto._id}</td>
-              <td>{producto.nombre}</td>
-              <td>1</td>
-              <td>{producto.precio}</td>
+              <td className='itemPedido'>{producto._id}</td>
+              <td className='itemPedido'>{producto.nombre}</td>
+              <td className='itemPedido'>1</td>
+              <td className='itemPedido'>{producto.precio}</td>
               <td>
               <Button variant="danger" onClick={()=>{quitarProducto(producto)}}><FontAwesomeIcon className="botont" icon={faTrash}/></Button>{' '}
               </td>
