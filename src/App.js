2d89c722b4bb03100acc829d6f328609b0cd5120
Juan Menichetti <jpmenichetti13@gmail.com>
2022-10-13 00:48:56 -0300

    se agrego las rutas protegidas para administrador y crear producto

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Login from "./components/views/Login";
 import Error from "./components/views/Error";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Administrador from "./components/views/administrador/Administrador";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
 
 const App = () => {
   const [usuarioLogueado, setUsuarioLogueado] = useState({});
@@ -21,13 +22,19 @@ const App = () => {
         <Menu usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado} />
         <Routes>
           <Route exact path="/" element={<Home/>} />
-          <Route exact path="/administrador" element={<Administrador/>} />
+          <Route path='/*' element={
+            <RutasProtegidas>
+              <Routes>
+                <Route exact path="/administrador" element={<Administrador/>} />
+                <Route exact path="/crearProducto" element={<CrearProducto/>}/>
+              </Routes>
+            </RutasProtegidas>
+          }></Route>
           <Route exact path="/acercaDe" element={<AcercaDe/>} />
           <Route exact path="/detalleProducto/:id" element={<DetalleProducto/>} />
           <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}/>}/>
           <Route exact path="/registro" element={<Registro setUsuarioLogueado={setUsuarioLogueado}/>} />
           <Route exact path="*" element={<Error/>} />
-          <Route exact path="/crearProducto" element={<CrearProducto/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
