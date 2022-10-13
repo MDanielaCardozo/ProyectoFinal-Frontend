a164e2d592da2240a6496a0e574434f69ce74357
carlosmsx <carlosmsx@gmail.com>
2022-10-13 00:46:53 -0300

    agregada logica para agregar productos al carrito, quitar productos del carrito

--- src/components/views/ItemPedidos.js
+++ src/components/views/ItemPedidos.js
@@ -3,15 +3,15 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
 
-const ItemPedidos = (props) => {
+const ItemPedidos = ({producto, quitarProducto}) => {
     return (
          <tr>
-              <td>{props.producto._id}</td>
-              <td>{props.producto.nombre}</td>
+              <td>{producto._id}</td>
+              <td>{producto.nombre}</td>
               <td>1</td>
               <td>$850</td>
               <td>
-              <Button variant="danger"><FontAwesomeIcon className="botont" icon={faTrash}/></Button>{' '}
+              <Button variant="danger" onClick={()=>{quitarProducto(producto)}}><FontAwesomeIcon className="botont" icon={faTrash}/></Button>{' '}
               </td>
             </tr>
     );
--- src/components/views/Pedidos.js
+++ src/components/views/Pedidos.js
@@ -1,78 +1,56 @@
-import React ,{useEffect, useState} from "react";
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import Swal from 'sweetalert2';
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Swal from "sweetalert2";
 import ItemPedidos from "./ItemPedidos";
 import "./Pedidos.css";
 
-
 const Pedidos = () => {
-  
-  useEffect(() => {
-    consultarAPI();
-  }, []);
-  
-  const [productos, setProductos] = useState([]);
-  
-  const consultarAPI = async () => {
-    try {
-      const respuesta = await fetch(URL);
-      const listaProductos = await respuesta.json();
-      setProductos(listaProductos);
-    } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Algo falló",
-        text: "Intenta esto más tarde",
-      });
+    const productosPedido = JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_PRODUCTOS_PEDIDO)) || [];
+    const [listaProductosPedido, setListaProductosPedido] = useState(productosPedido);
+
+    const quitarProducto=(producto)=>{
+      let nuevaLista = listaProductosPedido.filter((item)=>{return item._id !== producto._id});
+      setListaProductosPedido( nuevaLista );
+      localStorage.setItem(process.env.REACT_APP_LOCALSTORAGE_PRODUCTOS_PEDIDO, JSON.stringify(nuevaLista));
+      Swal.fire(
+        'Producto eliminado',
+        'El producto fue quitado del pedido',
+        'success'
+      );
     }
-  };
   
-  const handleClick = ()=>{
-    Swal.fire(
-      'Muy bien!',
-      'Su producto esta siendo preparado.',
-      'success'
-    )
-  }
-  return (
-    <div className="fondo text-center text-dark">
-      <h1 className="display-3 text-light">CARRITO DE COMPRAS</h1>
-      <div className="tabla container py-3">
-        <Table striped bordered responsive>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Producto</th>
-              <th>Cantidad</th>
-              <th>Precio</th>
-              <th>Accion</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* ejemplo */}
-            {/* <tr>
-              <td>1</td>
-              <td>CheesBurger</td>
-              <td>1</td>
-              <td>$850</td>
-              <td>
-              <Button variant="danger"><FontAwesomeIcon className="botont" icon={faTrash}/></Button>{' '}
-              </td>
-            </tr> */}
-            {productos.map((producto)=>(
-            <ItemPedidos
-            key = {producto._id}
-            producto = {producto}></ ItemPedidos>
-          ))}
-          </tbody>
-        </Table>
-          <div className="text-end">
-          <Button variant="primary" className='mt-3' onClick={handleClick}>Proceder a pagar</Button>
-          </div>
-      </div>
-    </div>
-  );
+    const handleClick = () => {
+        Swal.fire("Muy bien!", "Su producto esta siendo preparado.", "success");
+    };
+    return (
+        <div className="fondo text-center text-dark">
+            <h1 className="display-3 text-light">CARRITO DE COMPRAS</h1>
+            <div className="tabla container py-3">
+                <Table striped bordered responsive>
+                    <thead>
+                        <tr>
+                            <th>Item</th>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                            <th>Accion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listaProductosPedido.map((producto) => (
+                            <ItemPedidos key={producto._id} producto={producto} quitarProducto={quitarProducto}></ItemPedidos>
+                        ))}
+                    </tbody>
+                </Table>
+                <div className="text-end">
+                    <Button variant="primary" className="mt-3" onClick={handleClick}>
+                        Proceder a pagar
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Pedidos;
